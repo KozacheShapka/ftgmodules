66ffcd5bd28957b8a0d50e8db83a80487ced497e
KozacheShapka <79427139+KozacheShapka@users.noreply.github.com>
2021-07-09 00:14:01 +0300

    Update UniBorg5.py

--- UniBorg5.py
+++ UniBorg5.py
@@ -7,16 +7,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("tagall"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Bhai log"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, 100):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.reply(mentions)
-    await event.delete()
+
 
 
 @borg.on(admin_cmd("admin"))
