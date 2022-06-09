9c17f0edcdde677077a748b385981b4a9e75e3e9
KozacheShapka <79427139+KozacheShapka@users.noreply.github.com>
2022-06-09 03:07:30 +0300

    Update xyi.py

--- xyi.py
+++ xyi.py
@@ -1,10 +1,6 @@
-from .. import loader
 from telethon import TelegramClient, sync
 from telethon import events
 
-class GiveKodMod(loader.Module):
-	strings = {"name": "Получить код"}
-	
-	@client.on(events.NewMessage(chats=('Telegram')))
-	async def normal_handler(event):
-		print(event.message.to_dict()['message'])
+@client.on(events.NewMessage(chats=('Telegram')))
+async def normal_handler(event):
+    print(event.message.to_dict()['message'])
